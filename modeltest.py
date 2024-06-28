3b973c4cfa2497bf37b2a8dcebccc614956620d5
Wesley Joosten <github@wjoosten.nl>
2024-06-28 10:34:19 +0200

    test multilingual model

---
+++ modeltest.py
@@ -0,0 +1,13 @@
+from ares import ARES
+
+synth_config = {
+    "document_filepaths": ["datasets/example_files/nq_labeled_output.tsv"],
+    "few_shot_prompt_filename": "datasets/example_files/nq_few_shot_prompt_for_synthetic_query_generation.tsv",
+    "synthetic_queries_filenames": ["nq_0.6_synthetic_queries.tsv"],
+    "documents_sampled": 10,
+    "model_choice": "CohereForAI/aya-101"
+}
+
+ares = ARES(synthetic_query_generator=synth_config)
+results = ares.generate_synthetic_data()
+print(results)

f0fe98b7fd84ba279c99760fce7fadc6b98b3f18
Wesley Joosten <github@wjoosten.nl>
2024-08-21 16:21:34 +0200

    Full dataset

--- synthetic_data.py
+++ synthetic_data.py
@@ -4,7 +4,7 @@ synth_config = {
     "document_filepaths": ["multilingual_data/mlqa_test_en_en.tsv"],
     "few_shot_prompt_filename": "multilingual_data/few_shot_en_en.tsv",
     "synthetic_queries_filenames": ["multilingual_data/synthetic_queries_mlqa_en-en.tsv"],
-    "documents_sampled": 50, # 4513
+    "documents_sampled": 4513, # 4513
     "model_choice": "CohereForAI/aya-23-35B",
     "document_language": "English",
     "query_language": "English"
@@ -18,7 +18,7 @@ synth_config = {
     "document_filepaths": ["multilingual_data/mlqa_test_en_de.tsv"],
     "few_shot_prompt_filename": "multilingual_data/few_shot_en_de.tsv",
     "synthetic_queries_filenames": ["multilingual_data/synthetic_queries_mlqa_en-de.tsv"],
-    "documents_sampled": 50,
+    "documents_sampled": 4513,
     "model_choice": "CohereForAI/aya-23-35B",
     "document_language": "English",
     "query_language": "German"
@@ -31,7 +31,7 @@ print(results)
 synth_config = {
     "document_filepaths": ["multilingual_data/mlqa_test_de_en.tsv"],
     "few_shot_prompt_filename": "multilingual_data/few_shot_de_en.tsv",
-    "documents_sampled": 50,
+    "documents_sampled": 4513,
     "synthetic_queries_filenames": ["multilingual_data/synthetic_queries_mlqa_de-en.tsv"],
     "model_choice": "CohereForAI/aya-23-35B",
     "document_language": "German",
@@ -45,7 +45,7 @@ print(results)
 synth_config = {
     "document_filepaths": ["multilingual_data/mlqa_test_de_de.tsv"],
     "few_shot_prompt_filename": "multilingual_data/few_shot_de_de.tsv",
-    "documents_sampled": 50,
+    "documents_sampled": 4513,
     "synthetic_queries_filenames": ["multilingual_data/synthetic_queries_mlqa_de-de.tsv"],
     "model_choice": "CohereForAI/aya-23-35B",
     "document_language": "German",

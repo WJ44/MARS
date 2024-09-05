e810da4a0c0cde8df3431deea990bfcccfbc3e8a
Wesley Joosten <github@wjoosten.nl>
2024-09-05 11:53:55 +0200

    Cleanup

--- mars/synthetic_data_mlqa.py
+++ mars/synthetic_data_mlqa.py
@@ -3,7 +3,7 @@ from ares import ARES
 synth_config = {
     "document_filepaths": ["multilingual_data/mlqa_test_en_en.tsv"],
     "few_shot_prompt_filename": "multilingual_data/few_shot_en_en.tsv",
-    "synthetic_queries_filenames": ["multilingual_data/synthetic_queries_mlqa_en-en.tsv"],
+    "synthetic_queries_filenames": ["multilingual_data/synthetic_queries_mlqa_test_en-en.tsv"],
     "documents_sampled": 4513, # 4513
     "model_choice": "CohereForAI/aya-23-35B",
     "document_language": "English",
@@ -17,7 +17,7 @@ print(results)
 synth_config = {
     "document_filepaths": ["multilingual_data/mlqa_test_en_de.tsv"],
     "few_shot_prompt_filename": "multilingual_data/few_shot_en_de.tsv",
-    "synthetic_queries_filenames": ["multilingual_data/synthetic_queries_mlqa_en-de.tsv"],
+    "synthetic_queries_filenames": ["multilingual_data/synthetic_queries_mlqa_test_en-de.tsv"],
     "documents_sampled": 4513,
     "model_choice": "CohereForAI/aya-23-35B",
     "document_language": "English",
@@ -32,7 +32,7 @@ synth_config = {
     "document_filepaths": ["multilingual_data/mlqa_test_de_en.tsv"],
     "few_shot_prompt_filename": "multilingual_data/few_shot_de_en.tsv",
     "documents_sampled": 4513,
-    "synthetic_queries_filenames": ["multilingual_data/synthetic_queries_mlqa_de-en.tsv"],
+    "synthetic_queries_filenames": ["multilingual_data/synthetic_queries_mlqa_test_de-en.tsv"],
     "model_choice": "CohereForAI/aya-23-35B",
     "document_language": "German",
     "query_language": "English"
@@ -46,7 +46,7 @@ synth_config = {
     "document_filepaths": ["multilingual_data/mlqa_test_de_de.tsv"],
     "few_shot_prompt_filename": "multilingual_data/few_shot_de_de.tsv",
     "documents_sampled": 4513,
-    "synthetic_queries_filenames": ["multilingual_data/synthetic_queries_mlqa_de-de.tsv"],
+    "synthetic_queries_filenames": ["multilingual_data/synthetic_queries_mlqa_test_de-de.tsv"],
     "model_choice": "CohereForAI/aya-23-35B",
     "document_language": "German",
     "query_language": "German"

693839b55acef61774853861bd74d127ec651d4d
Wesley Joosten <github@wjoosten.nl>
2024-09-06 12:44:28 +0200

    Fix mistake in mlqa index path

--- mars/build_mlqa_index.py
+++ mars/build_mlqa_index.py
@@ -5,7 +5,7 @@ import json
 # Saves the artice title for each passage, this information is not easily available in the dataset as published on Hugginface
 langs = ["en", "de"]
 for lang in langs:
-    with open(f"multilingual_data/MLQA_V1/train/train-context-{lang}-question-{lang}.json", "r") as f:
+    with open(f"multilingual_data/MLQA_V1/test/test-context-{lang}-question-{lang}.json", "r") as f:
         data = json.load(f)
 
     qa_article_map = {}

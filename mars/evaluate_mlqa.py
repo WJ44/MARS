2baea3917d9c58ef6e9d5df22e4194bc2fb68a49
Wesley Joosten <github@wjoosten.nl>
2024-09-23 14:02:58 +0200

    Save results

--- mars/evaluate_mlqa.py
+++ mars/evaluate_mlqa.py
@@ -1,12 +1,13 @@
 from ares import ARES
+import json
 
 for lang1, lang2 in [("en", "en"), ("de", "de"), ("de", "en"), ("en", "de")]:
     ppi_config = {
-        "evaluation_datasets": ["multilingual_data/mlqa_test_ratio_0.5_{lang1}_{lang2}.tsv", "multilingual_data/mlqa_test_ratio_0.525_{lang1}_{lang2}.tsv", "multilingual_data/mlqa_test_ratio_0.55_{lang1}_{lang2}.tsv", "multilingual_data/mlqa_test_ratio_0.575_{lang1}_{lang2}.tsv", "multilingual_data/mlqa_test_ratio_0.6_{lang1}_{lang2}.tsv", "multilingual_data/mlqa_test_ratio_0.625_{lang1}_{lang2}.tsv", "multilingual_data/mlqa_test_ratio_0.65_{lang1}_{lang2}.tsv", "multilingual_data/mlqa_test_ratio_0.675_{lang1}_{lang2}.tsv", "multilingual_data/mlqa_test_ratio_0.7_{lang1}_{lang2}.tsv"],
+        "evaluation_datasets": [f"multilingual_data/mlqa_test_ratio_0.5_{lang1}_{lang2}.tsv", f"multilingual_data/mlqa_test_ratio_0.525_{lang1}_{lang2}.tsv", f"multilingual_data/mlqa_test_ratio_0.55_{lang1}_{lang2}.tsv", f"multilingual_data/mlqa_test_ratio_0.575_{lang1}_{lang2}.tsv", f"multilingual_data/mlqa_test_ratio_0.6_{lang1}_{lang2}.tsv", f"multilingual_data/mlqa_test_ratio_0.625_{lang1}_{lang2}.tsv", f"multilingual_data/mlqa_test_ratio_0.65_{lang1}_{lang2}.tsv", f"multilingual_data/mlqa_test_ratio_0.675_{lang1}_{lang2}.tsv", f"multilingual_data/mlqa_test_ratio_0.7_{lang1}_{lang2}.tsv"],
         "checkpoints": ["checkpoints/microsoft-mdeberta-v3-base/Context_Relevance_Label_mlqa_dev_ratio_0.7_2024-09-09_09:47:37.pt", "checkpoints/microsoft-mdeberta-v3-base/Answer_Relevance_Label_mlqa_dev_ratio_0.7_2024-09-09_14:52:34.pt", "checkpoints/microsoft-mdeberta-v3-base/Language_Consistency_Label_mlqa_dev_ratio_0.7_2024-09-10_10:31:55.pt"],
         "rag_type": "question_answering",
         "labels": ["Context_Relevance_Label", "Answer_Relevance_Label", "Language_Consistency_Label"],
-        "gold_label_paths": ["multilingual_data/mlqa_dev_ratio_0.5_{lang1}_{lang2}.tsv"],
+        "gold_label_paths": [f"multilingual_data/mlqa_dev_ratio_0.5_{lang1}_{lang2}.tsv"],
         "model_choice": "microsoft/mdeberta-v3-base",
         "assigned_batch_size": 8,
     }
@@ -14,6 +15,7 @@ for lang1, lang2 in [("en", "en"), ("de", "de"), ("de", "en"), ("en", "de")]:
     ares_module = ARES(ppi=ppi_config)
     results = ares_module.evaluate_RAG()
     print(results)
+    json.dump(results, open(f"results_{lang1}_{lang2}.json", "w"))
 
 
 ppi_config = {
@@ -29,3 +31,4 @@ ppi_config = {
 ares_module = ARES(ppi=ppi_config)
 results = ares_module.evaluate_RAG()
 print(results)
+json.dump(results, open("results.json", "w"))

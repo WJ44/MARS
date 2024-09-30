068803a0d45ebf786e290c7a348a0a916ad234fa
Wesley Joosten <github@wjoosten.nl>
2024-09-30 17:06:51 +0200

    Updates to classifier training script

--- mars/classifier_training_mlqa.py
+++ mars/classifier_training_mlqa.py
@@ -1,15 +1,15 @@
 from ares import ARES
 
 classifier_config = {
-    "training_dataset": ["multilingual_data/synthetic_queries_mlqa_en-en.tsv", "multilingual_data/synthetic_queries_mlqa_en-de.tsv", "multilingual_data/synthetic_queries_mlqa_de-en.tsv", "multilingual_data/synthetic_queries_mlqa_de-de.tsv"],
-    "training_dataset_path": "multilingual_data/synthetic_queries_mlqa.tsv",
-    "validation_set": ["multilingual_data/mlqa_dev_ratio_0.7.tsv"],
-    "label_column": ["Context_Relevance_Label", "Answer_Relevance_Label", "Language_Consistency_Label"],
+    "training_dataset": ["multilingual_data/synthetic_queries_mlqa_test_en-en.tsv", "multilingual_data/synthetic_queries_mlqa_test_en-de.tsv", "multilingual_data/synthetic_queries_mlqa_test_de-en.tsv", "multilingual_data/synthetic_queries_mlqa_test_de-de.tsv"],
+    "training_dataset_path": "multilingual_data/synthetic_queries_mlqa_test.tsv",
+    "validation_set": ["multilingual_data/mlqa_dev_ratio_0.5.tsv"],
+    "label_column": ["Context_Relevance_Label", "Answer_Relevance_Label", "Answer_Faithfulness_Label", "Language_Consistency_Label"],
     "model_choice": "microsoft/mdeberta-v3-base",
     "num_epochs": 10,
     "patience_value": 3,
     "learning_rate": 5e-6,
-    "assigned_batch_size": 1,
+    "assigned_batch_size": 32,
     "gradient_accumulation_multiplier": 32,
 }
 

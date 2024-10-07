87109ccc82d2cff126086e6279b480bfb00a952e
Wesley Joosten <github@wjoosten.nl>
2024-10-07 12:36:57 +0200

    Chang cora baseline gold labels to mlqa

--- baselines/cora.py
+++ baselines/cora.py
@@ -33,7 +33,7 @@ ppi_config = {
     "checkpoints": ["checkpoints/microsoft-mdeberta-v3-base/Context_Relevance_Label_mlqa_dev_ratio_0.5_2024-09-30_15:27:39.pt", "checkpoints/microsoft-mdeberta-v3-base/Answer_Relevance_Label_mlqa_dev_ratio_0.5_2024-10-01_07:58:16.pt", "checkpoints/microsoft-mdeberta-v3-base/Answer_Faithfulness_Label_mlqa_dev_ratio_0.5_2024-10-02_05:48:52.pt", "checkpoints/microsoft-mdeberta-v3-base/Language_Consistency_Label_mlqa_dev_ratio_0.5_2024-10-02_13:18:21.pt"],
     "rag_type": "question_answering",
     "labels": ["Context_Relevance_Label", "Answer_Relevance_Label", "Answer_Faithfulness_Label", "Language_Consistency_Label"],
-    "gold_label_paths": ["multilingual_data/xor_attri_qa_dev.tsv"],
+    "gold_label_paths": ["multilingual_data/mlqa_dev_ratio_0.5.tsv"],
     "model_choice": "microsoft/mdeberta-v3-base",
     "assigned_batch_size": 32,
     "prediction_filepaths": ["baselines/cora_xorqa_ja_preds.tsv"],

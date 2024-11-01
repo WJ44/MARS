0b31ebc6b18f161abfea6c786e90278f29385017
Wesley Joosten <github@wjoosten.nl>
2024-11-01 15:07:15 +0100

    MLQA results

--- mars/evaluate_mlqa.py
+++ mars/evaluate_mlqa.py
@@ -2,24 +2,6 @@ from ares import ARES
 import json
 from itertools import product
 
-#TODO: TEMP
-LANGS = ["en", "de"]
-ppi_config = {
-    "evaluation_datasets": [f"multilingual_data/mlqa_test_ratio_0.5_{LANGS[1]}.tsv", f"multilingual_data/mlqa_test_ratio_0.525_{LANGS[1]}.tsv", f"multilingual_data/mlqa_test_ratio_0.55_{LANGS[1]}.tsv", f"multilingual_data/mlqa_test_ratio_0.575_{LANGS[1]}.tsv", f"multilingual_data/mlqa_test_ratio_0.6_{LANGS[1]}.tsv", f"multilingual_data/mlqa_test_ratio_0.625_{LANGS[1]}.tsv", f"multilingual_data/mlqa_test_ratio_0.65_{LANGS[1]}.tsv", f"multilingual_data/mlqa_test_ratio_0.675_{LANGS[1]}.tsv", f"multilingual_data/mlqa_test_ratio_0.7_{LANGS[1]}.tsv"],
-    "checkpoints": ["checkpoints/microsoft-mdeberta-v3-base/Context_Relevance_Label_mlqa_dev_ratio_0.5_2024-09-30_15:27:39.pt", "checkpoints/microsoft-mdeberta-v3-base/Answer_Relevance_Label_mlqa_dev_ratio_0.5_2024-10-01_07:58:16.pt", "checkpoints/microsoft-mdeberta-v3-base/Answer_Faithfulness_Label_mlqa_dev_ratio_0.5_2024-10-02_05:48:52.pt", "checkpoints/microsoft-mdeberta-v3-base/Language_Consistency_Label_mlqa_dev_ratio_0.5_2024-10-02_13:18:21.pt"],
-    "rag_type": "question_answering",
-    "labels": ["Context_Relevance_Label", "Answer_Relevance_Label", "Answer_Faithfulness_Label", "Language_Consistency_Label"],
-    "gold_label_paths": [f"multilingual_data/mlqa_dev_ratio_0.5_{LANGS[1]}.tsv"],
-    "model_choice": "microsoft/mdeberta-v3-base",
-    "assigned_batch_size": 32,
-    "prediction_filepaths": [f"results_output_ratio_0.5_{LANGS[1]}.json", f"results_output_ratio_0.525_{LANGS[1]}.json", f"results_output_ratio_0.55_{LANGS[1]}.json", f"results_output_ratio_0.575_{LANGS[1]}.json", f"results_output_ratio_0.6_{LANGS[1]}.json", f"results_output_ratio_0.625_{LANGS[1]}.json", f"results_output_ratio_0.65_{LANGS[1]}.json", f"results_output_ratio_0.675_{LANGS[1]}.json", f"results_output_ratio_0.7_{LANGS[1]}.json"],
-}
-
-ares_module = ARES(ppi=ppi_config)
-results = ares_module.evaluate_RAG()
-print(results)
-json.dump(results, open(f"results_{LANGS[1]}.json", "w"))
-
 LANGS = ["en", "ar"]
 
 for lang1, lang2 in product(LANGS, repeat=2):
---
+++ results/results.py
@@ -0,0 +1,64 @@
+import json
+import os
+import pandas as pd
+from scipy.stats import kendalltau
+import matplotlib.pyplot as plt
+import tikzplotlib
+
+path = "results"
+
+table = pd.DataFrame(columns=["Experiment", "Context Relevance", "Answer Relevance", "Answer Faithfulness", "Language Consistency"])
+table.set_index("Experiment", inplace=True)
+
+for filename in sorted(os.listdir(path)):
+    if filename.endswith(".json"):
+        experiment_name = filename.replace("results_", "").replace(".json", "").replace("_", " ")
+        file_path = os.path.join(path, filename)
+
+        table.loc[experiment_name] = [0.0, 0.0, 0.0, 0.0]
+
+        with open(file_path, 'r') as file:
+            results = json.load(file)
+
+        for experiment in results:
+            accuracies = []
+            scores = []
+            truths = []
+            confidences = []
+            for split in experiment:
+                accuracies.append(split["ARES_LLM_Judge_Accuracy_on_Ground_Truth_Labels"])
+                scores.append(split["ARES_Prediction"])
+                truths.append(split["Ground_Truth_Performance"])
+                confidences.append(split["ARES_Confidence_Interval"])
+            tau, p_value = kendalltau(scores, truths)
+            print("Metric: ", experiment[0]["Label_Column"])
+            print("Average accuracy: ", sum(accuracies) / len(accuracies))
+            print("Kendall's tau: ", tau)
+
+            # Transpose confidences
+            confidences = list(map(list, zip(*confidences)))
+            confidences = [[abs(a - b) for a, b in list(zip(x, scores))] for x in confidences]
+            print("Confidence intervals: ", confidences)
+
+            label = experiment[0]["Label_Column"].replace("_", " ").replace(" Label", "")
+
+            plt.figure(figsize=(10, 6))
+
+            # Plot scores vs truths
+            plt.errorbar(truths, scores, yerr=confidences, fmt='o', ecolor='r', capthick=2, label='Scores with Confidence Intervals')
+            plt.scatter(truths, truths, color='g', marker='o', label='Ground Truth Performance')
+            plt.xlabel('Ground Truth Performance')
+            plt.ylabel('ARES Prediction')
+            plt.title(f'{experiment_name} - {label}')
+            plt.ylim(0, 1)
+            plt.legend()
+            plt.grid(True)
+            # plt.show()
+
+            plot_filename = f"plots/results_{experiment_name}_{experiment[0]['Label_Column']}.tex"
+            tikzplotlib.save(plot_filename)
+            print(f"Saved plot to {plot_filename}")
+
+            table.loc[experiment_name][label] = tau
+        
+print(table.to_latex(float_format="%.2f"))
